ed74ef00270333f1a04b8af2d3d8d5f2a4c6ea6c
Kevin Smith <sevinkmith1996@gmail.com>
2022-03-09 11:24:37 +0000

    Made change to checker function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,11 @@ function checker(input){
     var choices = ["rock", "paper", "scissors", "lizard", "spock"];
     var num = Math.floor(Math.random()*5);
 
-    document.getElementById("pc-choice").
-    innerHTML = `Computer chose <span>${choices[num].toUpperCase()}</span>`;
+    const pcChoice = document.getElementById("pc-choice").
+    pcChoice.innerHTML = `Computer chose <span>${choices[num].toUpperCase()}</span>`;
 
-    document.getElementByID("user-choice").
-    innerHTML= `You chose <span> ${input.toUpperCase()} </span>`;
+    const userChoice = document.getElementById("user-choice").
+    userChoice.innerHTML= `You chose <span> ${input.toUpperCase()} </span>`;
 }
 
 checker();
