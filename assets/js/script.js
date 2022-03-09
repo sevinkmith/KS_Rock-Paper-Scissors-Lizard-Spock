95d180e05cc5bdab8ce215143e95410bea766b8a
Kevin Smith <sevinkmith1996@gmail.com>
2022-03-09 21:14:29 +0000

    Change of js to add event listener and loop for computer choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
-function checker(input){
-    var choices = ["rock", "paper", "scissors", "lizard", "spock"];
-    var num = Math.floor(Math.random()*5);
+const controls = document.getElementsByClassName("control");
+const userChoice = document.getElementById("user-choice");
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
-    const pcChoice = document.getElementById("pc-choice");
-    pcChoice.innerHTML = `Computer chose <span>${choices[num].toUpperCase()}</span>`;
-
-    const userChoice = document.getElementById("user-choice")
-    userChoice.innerHTML= `You chose <span> ${input.toUpperCase()} </span>`;
+for (let control of controls) {
+    controls.addEventListener("click", function () {
+	let userChoice = this.getAttribute("data-choice")
+	playGame(userChoice);
+});
 }
+
+function playGame(userChoice) {
+    let pcChoice = document.getElementById("pc-choice");
+}	
