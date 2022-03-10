02642a41521dc498df9a353b93ac53640f6aa297
Kevin Smith <sevinkmith1996@gmail.com>
2022-03-10 10:47:39 +0000

    Add choices object variable to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,49 @@
-const controls = document.getElementsByClassName("control");
-const userChoice = document.getElementById("user-choice");
-const choices = ["rock", "paper", "scissors", "lizard", "spock"];
-
-for (let control of controls) {
-    controls.addEventListener("click", function () {
-	let userChoice = this.getAttribute("data-choice")
-	playGame(userChoice);
-});
+let resultInfo = document.getElementById("result");
+let choicesObject = {
+    'rock' : {
+        'rock' : 'draw',
+        'paper' : 'lose',
+        'scissors' : 'win',
+        'lizard' : 'win',
+        'spock' : 'lose'
+    },
+    'scissors' : {
+        'rock' : 'lose',
+        'paper' : 'win',
+        'scissors' : 'draw',
+        'lizard' : 'win',
+        'spock' : 'lose'
+    },
+    'paper' : {
+        'rock' : 'wins',
+        'paper' : 'draw',
+        'scissors' : 'lose',
+        'lizard' : 'lose',
+        'spock' : 'win'
+    },
+    'lizard' : {
+        'rock' : 'lose',
+        'paper' : 'wins',
+        'scissors' : 'lose',
+        'lizard' : 'draw',
+        'spock' : 'win'
+    },
+    'spock' : {
+        'rock' : 'win',
+        'paper' : 'lose',
+        'scissors' : 'wins',
+        'lizard' : 'lose',
+        'spock' : 'draw'
+    }
 }
+function checker(input){
+    var choices = ["rock", "paper", "scissors", "lizard", "spock"];
+    var num = Math.floor(Math.random()*5);
 
-function playGame(userChoice) {
-    let pcChoice = document.getElementById("pc-choice");
-}	
+    const pcChoice = document.getElementById("pc-choice");
+    pcChoice.innerHTML = `Computer chose <span>${choices[num].toUpperCase()}</span>`;
+
+    const userChoice = document.getElementById("user-choice")
+    userChoice.innerHTML= `You chose <span> ${input.toUpperCase()} </span>`;
+}
+checker();
